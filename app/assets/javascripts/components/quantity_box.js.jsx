f15a43eaed334ca51e2f4d1f6eb18a0032c4558c
Paul Maltsev <maltsevp@gmail.com>
2017-07-08 19:01:41 -0700

    styling products page

---
+++ app/assets/javascripts/components/quantity_box.js.jsx
@@ -0,0 +1,21 @@
+var QuantityBox = React.createClass({
+  getInitialState: function() {
+    return { value: 1 };
+  },
+  onDecrement: function(e) {
+    if (this.state.value <= 0) return;
+    this.setState({value: --this.state.value});
+  },
+  onIncrement: function(e) {
+    this.setState({value: ++this.state.value});
+  },    
+  render: function() {
+    return (
+      <div className="qty-box">
+        <span className="dec" onClick={this.onDecrement} onTouchStart={this.onDecrement}>–</span>
+        <span className="qty">{this.state.value}</span>
+        <span className="inc" onClick={this.onIncrement} onTouchStart={this.onIncrement}>+</span>
+      </div>
+    );
+  }
+});
